7f53268a39c03e9ca6d151e801f815fff760e60b
dxs <glaglasven@live.com>
2017-05-09 18:39:53 +0200

    Example 1 working

--- IrisDataset/V2/Iris2.py
+++ IrisDataset/V2/Iris2.py
@@ -31,12 +31,67 @@ print(dataset.groupby('class').size())
 
 #boxplot
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
+#plt.show()
 
 #histograms
 dataset.hist()
-plt.show()
+#plt.show()
 
 #scatter plot matrix
 scatter_matrix(dataset)
+#plt.show()
+
+#Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+#test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+#Let’s evaluate 6 different algorithms:
+#Logistic Regression (LR)
+#Linear Discriminant Analysis (LDA)
+#K-Nearest Neighbors (KNN).
+#Classification and Regression Trees (CART).
+#Gaussian Naive Bayes (NB).
+#Support Vector Machines (SVM).
+#This is a good mixture of simple linear (LR and LDA), nonlinear (KNN, CART, NB and SVM) algorithms. We reset the random number seed before each run to ensure that the evaluation of each algorithm is performed using exactly the same data splits. It ensures the results are directly comparable.
+
+#Spot check algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN',KNeighborsClassifier()))
+models.append(('CART',DecisionTreeClassifier()))
+models.append(('NB',GaussianNB()))
+models.append(('SVM',SVC()))
+#evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10,random_state=seed)
+    cv_result = model_selection.cross_val_score(model, X_train,Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_result)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_result.mean(), cv_result.std())
+    print(msg)
+fig = plt.figure()
+fig.suptitle('Algorith Comparaison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
 plt.show()
+
+#Make predictions on validation dataset
+
+knn = KNeighborsClassifier()
+knn.fit(X_train,Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
